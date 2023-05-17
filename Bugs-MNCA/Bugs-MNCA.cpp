ef7cffcf83f75583b0c44f8ff85c8c93c69a40d6
Jay Motamarry <jmotamarry@gmail.com>
2023-05-16 19:50:37 -0700

    random

--- Bugs-MNCA/Bugs-MNCA.cpp
+++ Bugs-MNCA/Bugs-MNCA.cpp
@@ -1,7 +1,35 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+#define SIZE_X 1920
+#define SIZE_Y 1080
 
 int main()
 {
-    printf("Hello world");
+    sf::err().rdbuf(NULL);
+    sf::RenderWindow window(sf::VideoMode(SIZE_X, SIZE_Y), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.draw(shape);
+        window.clear();
+
+        for (int x = 0; x < SIZE_X; x++) {
+            for (int y = 0; y < SIZE_Y; y++) {
+                sf::Vertex point(sf::Vector2f(x, y), sf::Color(rand() % 255, rand() % 255, rand() % 255, 255));
+                window.draw(&point, 1, sf::Points);
+            }
+        }
+        window.display();
+    }
+
+    return 0;
 }
