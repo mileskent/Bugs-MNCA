e28ac32f6807f5ccf8800f9fe7f977e8f88a1ba7
Miles Kent <mileskent13@gmail.com>
2023-05-25 11:03:46 -0700

    to be converted to MNCA

--- Bugs-MNCA/Bugs-MNCA.cpp
+++ Bugs-MNCA/Bugs-MNCA.cpp
@@ -1,63 +1,156 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <time.h>
-#include <stdlib.h>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	sf::err().rdbuf(NULL);
-	const float winW = 800;
-	const float winH = 800;
+#define resolution 200
 
 
-	sf::RenderWindow window(sf::VideoMode(winW, winH), "Bugs-MNCA");
-	// window.setMouseCursorVisible(false); // hide the cursor
-	window.setFramerateLimit(60);
+using namespace std;
+using namespace sf;
 
-	// Create a texture and a sprite for the shader
-	sf::Texture tex;
-	tex.create(winW, winH);
-	sf::Sprite spr(tex);
+int main()
+{
+    bool drag = false;
+    Glsl::Vec2 current_offset;
+    Glsl::Vec2 dragOrigin;
+    long long generation = 0;
+    float zoom = 1.0;
+    Clock uptime;
+    Event event;
 
-	sf::Uint8 * pixels = new sf::Uint8[winH * winW];
+    RenderWindow* window = new RenderWindow(VideoMode(resolution, resolution), "Game of Life GLSL");
+    window->setFramerateLimit(60);
 
-	for (size_t k = 0; k < winH * winW; k += 4)
-	{
-		sf::Uint8 val = rand() % 256;
-		*(pixels + k) = val;
-		*(pixels + k + 1) = *(pixels + k);
-		*(pixels + k + 2) = *(pixels + k);
-		*(pixels + k + 3) = 255;
-	}
+    VertexArray canvas(Quads, 4);
+    canvas[0] = Vertex(Vector2f(0, 0), Color::White, Vector2f(0, 0));
+    canvas[1] = Vertex(Vector2f(0, resolution), Color::White, Vector2f(0, resolution));
+    canvas[2] = Vertex(Vector2f(resolution, resolution), Color::White, Vector2f(resolution, resolution));
+    canvas[3] = Vertex(Vector2f(resolution, 0), Color::White, Vector2f(resolution, 0));
 
-	tex.update(pixels);
-	spr.setTexture(tex);
-	delete[] pixels;
+    RenderTexture* nextGen = new RenderTexture;
+    nextGen->create(resolution, resolution, ContextSettings());
+    nextGen->setRepeated(true);
 
-	sf::Shader shader;
+    RenderTexture* currentGen = new RenderTexture;
+    currentGen->create(resolution, resolution, ContextSettings());
+    currentGen->setRepeated(true);
+
+    Image* initState = new Image;
+    initState->create(resolution, resolution);
+    for (Uint8 y = 0; y < resolution; y++)
+    {
+        for (Uint8 x = 0; x < resolution; x++)
+        {
+            unsigned char color = (rand() % 2) * 255;
+            initState->setPixel(x, y, Color(color, color, color, 255));
+        }
+    }
+
+    Texture* upload = new Texture;
+    upload->loadFromImage(*initState);
+    currentGen->draw(canvas, upload);
+    currentGen->display();
+
+    delete upload;
+    delete initState;
+
+    Shader shader;
+    shader.loadFromFile("fragment.glsl", Shader::Fragment);
+    shader.setUniform("resolution", float(resolution));
+
+    Shader dispShader;
+    dispShader.loadFromFile("display_fragment.glsl", Shader::Fragment);
+    dispShader.setUniform("zoom", zoom);
+
+    RenderStates states;
+    states.shader = &shader;
+    states.texture = &currentGen->getTexture();
+
+    RenderStates dispStates;
+    dispStates.shader = &dispShader;
+
+    float start = 0;
+    float end = 0;
+
+    while (window->isOpen())
+    {
+
+        while (window->pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case Event::Closed:
+                window->close();
+                break;
+            case Event::MouseWheelScrolled:
+                zoom += event.mouseWheelScroll.delta * .1;
+                zoom = zoom < .25f ? .25f : zoom;
+                dispShader.setUniform("zoom", zoom);
+                break;
+            case Event::MouseButtonPressed:
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    drag = true;
+                    dragOrigin = Glsl::Vec2(Mouse::getPosition());
+                }
+                break;
+            case Event::MouseButtonReleased:
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    drag = false;
+                    dragOrigin = Glsl::Vec2(0, 0);
+                }
+                break;
+            default:
+                break;
+
+            }
+        }
 
 
-	if (!sf::Shader::isAvailable() || !shader.loadFromFile(".\\mnca.glsl", sf::Shader::Fragment))
-	{
-		std::cout << "Error loading shader!\n";
-		return 1;
-	}
+        //"Draw" the current generation to the next generation.
+        //The Game of Life simulation is happening here.
+        nextGen->draw(canvas, states);
+        nextGen->display();
 
-	shader.setUniform("u_resolution", sf::Vector2f(winW, winH));
+        //Now swap the pointers to the Renderbuffers, so we do not need to copy any
+        //texture data around.
+        RenderTexture* swap = currentGen;
+        currentGen = nextGen;
+        nextGen = swap;
 
-	while (window.isOpen())
-	{
-		sf::Event event;
+        //if the left mousebutton is pressed, we need to calculate the scrolling offset
+        if (drag)
+        {
+            Glsl::Vec2 offset;
+            offset = Glsl::Vec2(Mouse::getPosition());
+            offset = dragOrigin - offset;
+            offset.y *= -1;
+            offset.x /= resolution;
+            offset.y /= resolution;
+            current_offset += offset;
+            dragOrigin = Glsl::Vec2(Mouse::getPosition());
+        }
+        //Prepare the textures and uniforms, so they can be drawn to our window
+        dispStates.texture = &currentGen->getTexture();
+        dispShader.setUniform("offset", current_offset);
 
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::KeyPressed)
-				window.close();
-		}
 
-		shader.setUniform("u_time", (float)clock());
-		window.clear();
-		window.draw(spr, &shader);
-		window.display();
-	}
+        //Now we can draw everything
+        window->draw(canvas, dispStates);
+        window->display();
 
-	return 0;
+
+        //Add our text and update the frametimes
+        end = uptime.getElapsedTime().asSeconds();
+
+        start = uptime.getElapsedTime().asSeconds();
+        generation++;
+    }
+
+    //Clean up after yourself. ;)
+    delete currentGen;
+    delete nextGen;
+    delete window;
+
+    return 0;
 }
